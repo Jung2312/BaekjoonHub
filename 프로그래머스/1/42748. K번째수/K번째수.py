c349d0537792aac3c96ae83981f4393d8a1cdff1
JUNG <97083703+Jung2312@users.noreply.github.com>
2026-04-08 19:04:54 +0900

    [level 1] Title: K번째수, Time: 0.00 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,14 +1,10 @@
 def solution(array, commands):
     answer = []
     
-    for i in commands:
-        num1 = i[0] - 1
-        num2 = i[1]
-        num3 = i[2] - 1
-        
-        arrList = array[num1:num2]
+    for i, j, k in commands:
+        arrList = array[i - 1:j]
         arrList.sort()
         
-        answer.append(arrList[num3])
+        answer.append(arrList[k - 1])
         
     return answer
