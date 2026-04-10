950d0b3b0d73180ebc4a73d89629a0c92baf49b6
JUNG <97083703+Jung2312@users.noreply.github.com>
2026-04-10 16:22:52 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,11 +1,11 @@
 def solution(n, lost, reserve):
-    lost_set = set(lost) - set(reserve) #잃어버린 사람 중에 여분 가진 사람 제거
-    reserve_set = set(reserve) - set(lost) #잃어버린 사람 중에 여분 가진 사람 제거
+    lost_set = set(lost) - set(reserve)
+    reserve_set = set(reserve) - set(lost)
     
     for i in reserve_set:
         if i - 1 in lost_set:
             lost_set.remove(i-1)
         elif i + 1 in lost_set:
             lost_set.remove(i+1)
-            
+        
     return n - len(lost_set)
