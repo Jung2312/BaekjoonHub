732878b7df18e1081faefe81c7727cfbb5f4b9c1
JUNG <97083703+Jung2312@users.noreply.github.com>
2026-04-27 14:27:48 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 154.71 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,5 +1,10 @@
 import collections as col
 
 def solution(participant, completion):
+    
+    participant.sort()
+    completion.sort()
+    
     answer = col.Counter(participant) - col.Counter(completion)
+
     return list(answer.keys())[0]
